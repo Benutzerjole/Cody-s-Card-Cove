4c5ca69150d0fe920eb31f5073d62a4335a066ac
Joel <jvicente@aceproject.space>
2022-03-29 12:10:41 -0500

    Created all products page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "main#index"
+
+  get "/products", to: "products#index"
   
   # Defines the root path route ("/")
   # root "articles#index"
