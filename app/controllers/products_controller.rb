6ea4b0f5e9bc54f40bbc608acb56ae281152f74a
Joel <jvicente@aceproject.space>
2022-04-06 08:03:01 -0500

    Added pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,24 @@
 class ProductsController < ApplicationController
   def index 
+    
+    if !request.GET['page']
+      params[:page] = 1
+    end
+    
     if request.GET['search']
       products = Product.where("lower(name) LIKE (?) or lower(description) LIKE (?)", "%#{params[:search].downcase}%", "%#{params[:search].downcase}%")
       
-      if request.GET['category'] && request.GET['category'] != "All"
-        products = products.where("category_id = ?", params[:category])
-      end
+    if request.GET['category'] && request.GET['category'] != "All"
+      products = products.where("category_id = ?", params[:category])
+    end
       
       @products = products
     else
       @products = Product.all
     end
     @categories = Category.all
+    @pages = @products.count / 16 + 1
+    @products = @products.order(:name).page(params[:page].to_i)
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  paginates_per 16
+  
   belongs_to :category
   
   has_one_attached :image
