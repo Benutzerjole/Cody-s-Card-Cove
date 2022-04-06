c8081b6dd285ad682fb0e33e55d48dc0ce283d11
Joel <jvicente@aceproject.space>
2022-04-06 08:33:06 -0500

    Added filters to product search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,17 @@ class ProductsController < ApplicationController
     if request.GET['search']
       products = Product.where("lower(name) LIKE (?) or lower(description) LIKE (?)", "%#{params[:search].downcase}%", "%#{params[:search].downcase}%")
       
-    if request.GET['category'] && request.GET['category'] != "All"
-      products = products.where("category_id = ?", params[:category])
-    end
+      if request.GET['category'] && request.GET['category'] != "All"
+        products = products.where("category_id = ?", params[:category])
+      end
+
+      if request.GET['filter'] == "On Sale"
+        products = products.where('sale_price IS NOT NULL')
+      end
+
+      if request.GET['filter'] == "New Products"
+        products = products.where('created_at > ?', 3.days.ago)
+      end
       
       @products = products
     else
