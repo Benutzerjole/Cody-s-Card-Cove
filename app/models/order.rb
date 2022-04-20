03edb3288a0a12756c8e66fb0e3edff013b4800b
Joel <jvicente@aceproject.space>
2022-04-20 02:54:41 -0500

    Created order_products model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :province
   belongs_to :user
+  has_many :order_products
+  has_many :products, through: :order_products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   paginates_per 16
   
   belongs_to :category
+  has_many :order_products
+  has_many :orders, through: :order_products
   
   has_one_attached :image
 end
