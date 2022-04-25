dd2cf7ee83b7fe1d55b8bf432365a64fd4f23a48
Joel <jvicente@aceproject.space>
2022-04-25 14:17:58 -0500

    Fixing sale price bug

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,3 @@
-class SalePriceIsNumberValidator < ActiveModel::Validator
-  def validate(record)
-    return unless record.sale_price.is_a?(Numeric)
-
-    record.errors.add :base, "Sale price must be a number if it exists."
-  end
-end
-
 class ProductQuantityValidator < ActiveModel::Validator
   def validate(record)
     return unless record.quantity_in_stock.negative?
@@ -24,7 +16,6 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   validates_with ProductQuantityValidator
-  validates_with SalePriceIsNumberValidator
   validates :name, :base_price, :quantity_in_stock, presence: true
   validates :quantity_in_stock, :base_price, numericality: true
 end
